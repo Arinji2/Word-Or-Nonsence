0ea638f4a9d7368d4085d703e429a4e22b4a5075
Arinji2 <arinjaydhar205@gmail.com>
2023-05-13 01:29:20 +0530

    More Changes

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { genFakeWord, genOtherWords } from "../../utils/gameLogic";
+export const dynamic = "force-dynamic";
+
 import { gameGenFunc } from "../../utils/gameGenFunc";
 import * as React from "react";
 
@@ -9,32 +9,24 @@ interface Props {
   mode: string;
 }
 export default async function Home() {
-  const genRealFunc = async (words: string[]) => {
-    const data = await genOtherWords(words);
-    return data;
-  };
-  const genFakeFunc = async (words: string[]) => {
-    const data = await genFakeWord(words);
-    return data;
-  };
-
   const finalArr = (await gameGenFunc()) as any;
 
   return (
     <div className="w-full h-[100svh] bg-black flex flex-col items-center justify-center">
-      {finalArr.map((word: any, i: number) => (
-        <Card
-          word={word.word}
-          definition={word.definition}
-          mode={word.mode}
-          key={i}
-        />
-      ))}
+      {finalArr &&
+        finalArr.map((word: any, i: number) => (
+          <Card
+            word={word.word}
+            definition={word.definition}
+            mode={word.mode}
+            key={i}
+          />
+        ))}
     </div>
   );
 }
 
-const Card: React.FC<Props> = ({ word, definition, mode }) => {
+function Card({ word, definition, mode }: Props) {
   console.log(word, definition);
   return (
     <div className="flex flex-row items-center justify-center border-blue-300 border-2 m-2 p-2 ">
@@ -43,4 +35,4 @@ const Card: React.FC<Props> = ({ word, definition, mode }) => {
       <p className="z-30 text-white">Mode: {mode}</p>
     </div>
   );
-};
+}
